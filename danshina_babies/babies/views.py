f591fe6bccd3d0cd20236576f3918cfdd6c87de7
Artemigog <thisistyoma@gmail.com>
2022-09-10 00:06:35 +0300

    add api root to urls.py

--- danshina_babies/babies/views.py
+++ danshina_babies/babies/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import Http404, HttpResponseServerError
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import BabySerializer
+
+from loguru import logger
+
+
+"""class BabyDetail(APIView):
+    @staticmethod
+    def get_object(baby_id):
+        """
--- danshina_babies/danshina_babies/urls.py
+++ danshina_babies/danshina_babies/urls.py
@@ -5,5 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('api/v1/', include('djoser.urls')),
+    path('api/v1/', include('djoser.urls.authtoken')),
     path('api/v1/', include('babies.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

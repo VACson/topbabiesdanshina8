d94715a1e1ba0ced447388bacb0f70cd94072c01
Artemigog <thisistyoma@gmail.com>
2022-09-09 23:53:49 +0300

    added image crop method to Baby model

--- danshina_babies/babies/models.py
+++ danshina_babies/babies/models.py
@@ -2,6 +2,8 @@ from django.core.validators import MaxValueValidator, RegexValidator
 from django.db import models
 from io import BytesIO
 from PIL import Image
+from pathlib import Path
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 
 class Baby(models.Model):
@@ -16,7 +18,38 @@ class Baby(models.Model):
         db_table = 'baby'
 
     def __str__(self):
-        return str(f'{self.id}')
+        return str(f'id: {self.id}, rating: {self.rating}')
+
+    def save(self, *args, **kwargs):
+        super().save()
+        img = Image.open(self.image.path)
+        width, height = img.size
+
+        '''if width > 300 and height > 300:
+            # keep ratio but shrink down
+            img.thumbnail((width, height))'''
+
+        # check which one is smaller
+        if height < width:
+            # make square by cutting off equal amounts left and right
+            left = (width - height) // 2
+            right = (width + height) // 2
+            top = 0
+            bottom = height
+            img = img.crop((left, top, right, bottom))
+
+        elif width < height:
+            # make square by cutting off bottom
+            left = 0
+            right = width
+            top = 0
+            bottom = width
+            img = img.crop((left, top, right, bottom))
+
+        '''if width > 300 and height > 300:
+            img.thumbnail((300, 300))'''
+
+        img.save(self.image.path)
 
     def get_image(self):
         if self.image:

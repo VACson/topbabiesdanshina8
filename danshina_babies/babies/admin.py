2d7f371b31e8e7bb1f0d93e0cb18f3dff99590dc
Artemigog <thisistyoma@gmail.com>
2022-09-09 23:15:36 +0300

    add admin page for baby model

--- danshina_babies/babies/admin.py
+++ danshina_babies/babies/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Baby
+
+admin.site.site_header = 'Babies Admin'
+
+
+class BabyAdmin(admin.ModelAdmin):
+    model = Baby
+    extra = 1
+
+
+admin.site.register(Baby, BabyAdmin)

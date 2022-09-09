3c501deb0862108e01618f4adea68facf9d8334c
Artemigog <thisistyoma@gmail.com>
2022-09-09 23:14:53 +0300

    add Baby model

--- danshina_babies/babies/models.py
+++ danshina_babies/babies/models.py
@@ -1,10 +1,24 @@
-from pprint import pprint
-
-from django.http import Http404, HttpResponseServerError
-from rest_framework.decorators import api_view
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from django.core.validators import MaxValueValidator, RegexValidator
+from django.db import models
+from io import BytesIO
+from PIL import Image
 
 
-from loguru import logger
+class Baby(models.Model):
+    id = models.AutoField(primary_key=True)
+    is_confirmed = models.BooleanField(default=False)
+    description = models.CharField(max_length=255, null=True, blank=True)
+    rating = models.PositiveIntegerField(default=0, null=False)
+    image = models.ImageField(upload_to='uploads/babies/', null=False)
+    telegram_id = models.CharField(max_length=20, null=False)
 
+    class Meta:
+        db_table = 'baby'
+
+    def __str__(self):
+        return str(f'{self.id}')
+
+    def get_image(self):
+        if self.image:
+            return 'http://127.0.0.1:8000' + self.image.url
+        return ''

1a5b2c2481200e5cda1943e80f20d35870c37042
Artemigog <thisistyoma@gmail.com>
2022-09-10 23:32:43 +0300

    added url to new dashboard

--- danshina_babies/danshina_babies/urls.py
+++ danshina_babies/danshina_babies/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('jet/', include('jet.urls', 'jet')),
     path("admin/", admin.site.urls),
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.authtoken')),

228855b75ed2fb1ff3ff7b3b2c9280efa93f72f6
Artemigog <thisistyoma@gmail.com>
2022-09-11 13:19:46 +0300

    added new display method

--- danshina_babies/babies/admin.py
+++ danshina_babies/babies/admin.py
@@ -8,6 +8,14 @@ admin.site.site_header = 'Babies Admin'
 class BabyAdmin(admin.ModelAdmin):
     model = Baby
     extra = 1
+    list_display = ("id",
+                    "name",
+                    "telegram_username",
+                    "rating",
+                    "is_confirmed",
+                    )
+    list_filter = ('is_confirmed',)
+    readonly_fields = ('image_preview',)
 
 
 admin.site.register(Baby, BabyAdmin)

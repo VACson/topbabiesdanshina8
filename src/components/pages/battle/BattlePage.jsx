5973cc09be601d2f258b4a5c4540950665d3b493
Pablo <pablopukha@gmail.com>
2022-09-18 16:25:41 +0300

    Fix compile bug

--- src/components/pages/battle/BattlePage.jsx
+++ src/components/pages/battle/BattlePage.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import top1 from '../../../assets/img/top1.jpg';
+import top2 from '../../../assets/img/top2.jpg';
+import top3 from '../../../assets/img/top3.jpg';
 
 function BattlePage() {
-  return <div>BattlePage</div>;
+  return (
+      <div>
+        <div>
+          <img src="" alt=""/>
+          <img src="" alt=""/>
+        </div>
+      </div>
+  );
 }
 
 export default BattlePage;

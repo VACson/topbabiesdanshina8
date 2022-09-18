9f0786209dbe36f50dd0ab61e8a312452ef7e435
Pablo <pablopukha@gmail.com>
2022-09-18 18:30:09 +0300

    Add battle page

--- src/components/pages/battle/BattlePage.jsx
+++ src/components/pages/battle/BattlePage.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import top1 from '../../../assets/img/top1.jpg';
 import top2 from '../../../assets/img/top2.jpg';
 import top3 from '../../../assets/img/top3.jpg';
+import styles from './battle.module.scss';
 
 function BattlePage() {
   return (
       <div>
-        <div>
-          <img src="" alt=""/>
-          <img src="" alt=""/>
+        <div className={styles.wrapper}>
+            <div>
+                <img className={styles.photo} src={top3} alt=""/>
+                <div></div>
+            </div>
+            <div>
+                <img className={styles.photo} src={top2} alt=""/>
+                <div></div>
+            </div>
         </div>
       </div>
   );
